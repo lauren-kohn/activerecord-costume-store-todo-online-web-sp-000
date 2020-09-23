54ef3abacd50ad06cb6e000c0a1d39489ec87327
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-23 01:30:10 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,10 +5,11 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :name 
       t.string :location 
       t.string :theme
-      
-      t.datetime Date.new(:year, :month, :day)
-      
-      
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
     end 
   end 
   
