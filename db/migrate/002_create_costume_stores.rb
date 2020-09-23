d6e3eda75ded26e990e432e8f861c7a638c67ee8
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-23 01:03:04 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Migration[4.2]
+
+  def change 
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price 
+      t.text :image_url
+      t.string :size
+      t.timestamps
+    end
+  end 
+
+end
