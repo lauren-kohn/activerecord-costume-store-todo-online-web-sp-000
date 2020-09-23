cd1acdb17a35232b21b15698ed7df1220091b928
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-23 01:22:02 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change 
     create_table :haunted_houses do |t|
       t.string :name 
+      t.string :location 
+      t.string :theme
       
       
     end 
